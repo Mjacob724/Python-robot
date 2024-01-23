6f7ded5e8eff4c4b2d338f42a3cfe7609b20155e
tatayoyoh <tatayoyoh@mymail>
2024-01-23 10:05:11 +0100

    startup

---
+++ exo2/exo2_starter_template.py
@@ -0,0 +1,43 @@
+from Exo1 import Robot
+
+#class Robot():
+#    """ Robot class content here"""
+
+
+class Human:
+    def __init__(self, sexe):
+        self.sexe = sexe
+        self.estomac = []
+
+    def miam(self, manger):
+        if isinstance(manger, list):
+            self.estomac.extend(manger)
+        else:
+            self.estomac.append(manger)
+
+    def digest(self):
+        print('Digérer...')
+
+class Cyborg(Robot, Human):
+    def __init__(self, nom, sexe):
+        Robot.__init__(self, nom)
+        Human.__init__(self, sexe)
+    
+    # Méthode fun
+    def faire_la_danse_du_robot(self):
+        print(f'{self.nom} est en train de faire la danse du robot !')
+
+
+if __name__ == "__main__":
+
+    cyborg1 = Cyborg('Patrick', 'Homme')
+
+    print(cyborg1.nom, 'sexe', cyborg1.sexe)
+    print('Charging battery...')
+    cyborg1.allumer()
+    cyborg1.miam('banana')
+    cyborg1.miam(['coca', 'chips'])
+    print(cyborg1.estomac)
+    cyborg1.digest()
+
+    cyborg1.faire_la_danse_du_robot()
